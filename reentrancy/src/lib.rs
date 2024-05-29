eea1c0b009570baface72c9208ff029a55e5be57
silence <35656692+silence48@users.noreply.github.com>
2024-05-29 16:08:07 -0400

    use a vec;

--- reentrancy/src/lib.rs
+++ reentrancy/src/lib.rs
@@ -8,12 +8,20 @@ use crate::dependencies::soroswap::{SoroswapRouterClient};
 use crate::storage;
 use crate::error::PositionManagerError;
 
+#[derive(Clone, Debug)]
+struct PoolAddresses {
+    blend_pool: Address,
+    usdc: Address,
+    xlm: Address,
+    soroswap_router: Address,
+}
+
 #[contract]
 pub struct ReentrancyContract;
 
 #[contractclient(name = "ReentrancyClient")]
 pub trait Reentrancy {
-    fn initialize(e: Env, admin: Address, fee_taker: Address, blend_pool: Address, usdc: Address, xlm: Address, soroswap_router: Address);
+    fn initialize(e: Env, admin: Address, fee_taker: Address, pool_addresses: PoolAddresses);
 
     fn set_admin(e: Env, admin: Address);
 
@@ -21,20 +29,14 @@ pub trait Reentrancy {
 
     fn get_fee_taker(e: Env) -> Address;
 
-    fn get_pool(e: Env) -> Address;
-
-    fn get_usdc_address(e: Env) -> Address;
-
-    fn get_xlm_address(e: Env) -> Address;
-
-    fn get_soroswap_router_address(e: Env) -> Address;
+    fn get_pool_addresses(e: Env) -> PoolAddresses;
 
     fn execute_reentrancy(e: Env, user: Address, amount: i128, reenter_count: u32);
 }
 
 #[contractimpl]
 impl Reentrancy for ReentrancyContract {
-    fn initialize(e: Env, admin: Address, fee_taker: Address, blend_pool: Address, usdc: Address, xlm: Address, soroswap_router: Address) {
+    fn initialize(e: Env, admin: Address, fee_taker: Address, pool_addresses: PoolAddresses) {
         storage::extend_instance(&e);
         if storage::get_is_init(&e) {
             panic_with_error!(&e, PositionManagerError::AlreadyInitializedError);
@@ -42,10 +44,7 @@ impl Reentrancy for ReentrancyContract {
 
         storage::set_admin(&e, &admin);
         storage::set_fee(&e, &fee_taker);
-        storage::set_pool(&e, &blend_pool);
-        storage::set_usdc(&e, &usdc);
-        storage::set_xlm(&e, &xlm);
-        storage::set_soroswap_router(&e, &soroswap_router);
+        storage::set_pool_addresses(&e, &pool_addresses);
         storage::set_is_init(&e);
     }
 
@@ -70,27 +69,13 @@ impl Reentrancy for ReentrancyContract {
         storage::get_fee(&e)
     }
 
-    fn get_pool(e: Env) -> Address {
+    fn get_pool_addresses(e: Env) -> PoolAddresses {
         storage::extend_instance(&e);
-        storage::get_pool(&e)
-    }
-
-    fn get_usdc_address(e: Env) -> Address {
-        storage::extend_instance(&e);
-        storage::get_usdc(&e)
-    }
-
-    fn get_xlm_address(e: Env) -> Address {
-        storage::extend_instance(&e);
-        storage::get_xlm(&e)
-    }
-
-    fn get_soroswap_router_address(e: Env) -> Address {
-        storage::extend_instance(&e);
-        storage::get_soroswap_router(&e)
+        storage::get_pool_addresses(&e)
     }
 
     fn execute_reentrancy(e: Env, user: Address, amount: i128, reenter_count: u32) {
+        let pool_addresses = storage::get_pool_addresses(&e);
         let mut current_amount = amount;
         
         for _ in 0..reenter_count {
@@ -98,27 +83,27 @@ impl Reentrancy for ReentrancyContract {
             user.require_auth();
             let supply_request = Request {
                 request_type: RequestType::SupplyCollateral as u32,
-                address: get_usdc_address(&e),
+                address: pool_addresses.usdc.clone(),
                 amount: current_amount,
             };
             let mut requests = Vec::new(&e);
             requests.push_back(supply_request);
-            PoolClient::new(&e, &get_pool(&e)).submit(&user, &user, &user, &requests);
+            PoolClient::new(&e, &pool_addresses.blend_pool).submit(&user, &user, &user, &requests);
 
             // Step 2: Borrow USD from the pool
             let borrow_request = Request {
                 request_type: RequestType::Borrow as u32,
-                address: get_usdc_address(&e),
+                address: pool_addresses.usdc.clone(),
                 amount: current_amount / 2,  // Adjust the amount as needed
             };
             let mut borrow_requests = Vec::new(&e);
             borrow_requests.push_back(borrow_request);
-            PoolClient::new(&e, &get_pool(&e)).submit(&user, &user, &user, &borrow_requests);
+            PoolClient::new(&e, &pool_addresses.blend_pool).submit(&user, &user, &user, &borrow_requests);
 
             // Step 3: Swap USD for XLM on the AMM
             let swap_amount = current_amount / 2;  // Adjust the amount as needed
-            let path = vec![get_usdc_address(&e), get_xlm_address(&e)];
-            SoroswapRouterClient::new(&e, &get_soroswap_router_address(&e))
+            let path = vec![pool_addresses.usdc.clone(), pool_addresses.xlm.clone()];
+            SoroswapRouterClient::new(&e, &pool_addresses.soroswap_router)
                 .swap_exact_tokens_for_tokens(&swap_amount, &0, &path, &user, &u64::MAX);
 
             // Update the current amount for the next iteration
@@ -130,7 +115,7 @@ impl Reentrancy for ReentrancyContract {
 // Storage implementation for storing addresses
 mod storage {
     use soroban_sdk::{Address, Env, IntoVal, Symbol, panic_with_error};
-    use crate::error::PositionManagerError;
+    use crate::{PoolAddresses, error::PositionManagerError};
 
     pub fn extend_instance(e: &Env) {
         e.storage().extend();
@@ -160,36 +145,12 @@ mod storage {
         e.storage().set(&Symbol::new(e, "fee_taker"), fee_taker);
     }
 
-    pub fn get_pool(e: &Env) -> Address {
-        e.storage().get(&Symbol::new(e, "blend_pool")).unwrap()
+    pub fn get_pool_addresses(e: &Env) -> PoolAddresses {
+        e.storage().get(&Symbol::new(e, "pool_addresses")).unwrap()
     }
 
-    pub fn set_pool(e: &Env, pool: &Address) {
-        e.storage().set(&Symbol::new(e, "blend_pool"), pool);
-    }
-
-    pub fn get_usdc(e: &Env) -> Address {
-        e.storage().get(&Symbol::new(e, "usdc")).unwrap()
-    }
-
-    pub fn set_usdc(e: &Env, usdc: &Address) {
-        e.storage().set(&Symbol::new(e, "usdc"), usdc);
-    }
-
-    pub fn get_xlm(e: &Env) -> Address {
-        e.storage().get(&Symbol::new(e, "xlm")).unwrap()
-    }
-
-    pub fn set_xlm(e: &Env, xlm: &Address) {
-        e.storage().set(&Symbol::new(e, "xlm"), xlm);
-    }
-
-    pub fn get_soroswap_router(e: &Env) -> Address {
-        e.storage().get(&Symbol::new(e, "soroswap_router")).unwrap()
-    }
-
-    pub fn set_soroswap_router(e: &Env, router: &Address) {
-        e.storage().set(&Symbol::new(e, "soroswap_router"), router);
+    pub fn set_pool_addresses(e: &Env, pool_addresses: &PoolAddresses) {
+        e.storage().set(&Symbol::new(e, "pool_addresses"), pool_addresses);
     }
 }
 

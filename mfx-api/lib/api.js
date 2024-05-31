5e04cfcf9badbaf144d269371c91a9bf603aad17
Robin <70903912+Robeartt@users.noreply.github.com>
2024-05-31 09:45:50 -0500

    final fix

--- mfx-api/lib/api.js
+++ mfx-api/lib/api.js
@@ -21,7 +21,7 @@ app.post("/", async (req, res) => {
     console.log(json);
     const leveragePosition = new PositionManagerContract(contract);
     let amount = json.amount;
-    let amount2 = json.amount2;
+    let amount2 = Math.floor(json.amount2);
     console.log(json);
     const account = await config.rpc.getAccount(config.admin.publicKey());
     const sorobanOp = leveragePosition.pool_open_position(json.user, lend, borrow, BigInt(amount * SCALAR_7), BigInt(amount2 * SCALAR_7));
@@ -32,29 +32,33 @@ app.post("/", async (req, res) => {
         .addOperation(xdr.Operation.fromXDR(sorobanOp, 'base64'))
         .setTimeout(30)
         .build();
+    const simulationResult = await config.rpc.simulateTransaction(tx);
+    if (SorobanRpc.Api.isSimulationError(simulationResult)) {
+        console.log(simulationResult);
+        res.status(400).json({ error: simulationResult });
+        return;
+    }
+    const assembledTx = SorobanRpc.assembleTransaction(tx, simulationResult).build();
     res.json({
-        xdr: tx.toXDR(),
+        xdr: assembledTx.toXDR(),
     });
 });
 // New endpoint for submitting signed transactions
 app.post("/submit", async (req, res) => {
     const signedXdr = req.body;
     try {
-        const account = await config.rpc.getAccount(signedXdr.publicKey);
         //const transaction = new Transaction(signedXdr.signedXdr, Networks.TESTNET);
         const transaction = TransactionBuilder.fromXDR(signedXdr.signedXdr, Networks.TESTNET);
-        // Simulate the transaction
-        const simulationResult = await config.rpc.simulateTransaction(transaction);
-        if (SorobanRpc.Api.isSimulationError(simulationResult)) {
-            res.status(400).json({ error: simulationResult });
-            return;
-        }
-        const assembledTx = SorobanRpc.assembleTransaction(transaction, simulationResult).build();
         // Submit the transaction
-        const submitResult = await sendTransaction(assembledTx, PositionManagerContract.parsers.pool_open_position);
+        //@ts-ignore
+        const submitResult = await sendTransaction(transaction, PositionManagerContract.parsers.pool_open_position);
         console.log(submitResult);
         if (submitResult) {
-            res.json({ result: submitResult });
+            // Divide the BigInt by 10000000
+            const result = submitResult / BigInt(10000000);
+            // Convert to a regular number
+            const resultAsNumber = Number(result);
+            res.json({ result: resultAsNumber });
         }
         else {
             res.status(400).json({ error: submitResult });
